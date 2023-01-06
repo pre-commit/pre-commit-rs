6864ea4f6758c491a33ab30c9bc659a9de3b39ac
Anthony Sottile <asottile@umich.edu>
2023-01-06 14:19:26 -0500

    implement pre-commit sample-config

---
+++ src/commands/sample_config.rs
@@ -0,0 +1,16 @@
+static SAMPLE_CONFIG: &str = r"
+# See https://pre-commit.com for more information
+# See https://pre-commit.com/hooks.html for more hooks
+repos:
+-   repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v3.2.0
+    hooks:
+    -   id: trailing-whitespace
+    -   id: end-of-file-fixer
+    -   id: check-yaml
+    -   id: check-added-large-files
+";
+
+pub(crate) fn cmd() {
+    print!("{SAMPLE_CONFIG}")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+mod commands;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Produce a sample .pre-commit-config.yaml file
+    SampleConfig,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::SampleConfig) => {
+            return commands::sample_config::cmd();
+        }
+        None => {
+            println!("default")
+        }
+    }
 }
