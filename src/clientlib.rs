f16a54aa618f5df4b98a6f836ffcfc650ea8dd1b
Anthony Sottile <asottile@umich.edu>
2024-11-27 17:53:53 -0500

    partially implement repo: local / repo: meta validation

--- src/clientlib.rs
+++ src/clientlib.rs
@@ -56,11 +56,51 @@ pub(crate) struct ConfigHook;
 
 #[allow(dead_code)]
 #[derive(Cfgv, Debug)]
-pub(crate) struct Repo {
+pub(crate) struct LocalRepo {
+    #[cfgv_id]
+    repo: String,
+    hooks: Vec<ManifestHook>,
+}
+
+#[allow(dead_code)]
+#[derive(Cfgv, Debug)]
+pub(crate) struct MetaRepo {
+    #[cfgv_id]
+    repo: String,
+    hooks: Vec<ConfigHook>,
+}
+
+#[allow(dead_code)]
+#[derive(Cfgv, Debug)]
+pub(crate) struct RemoteRepo {
     #[cfgv_id]
     repo: String,
     rev: String,
     hooks: Vec<ConfigHook>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) enum Repo {
+    Local(LocalRepo),
+    Meta(MetaRepo),
+    Remote(RemoteRepo),
+}
+
+impl Cfgv for Repo {
+    fn cfgv_validate(ctx: &mut Vec<String>, v: &serde_yaml::Value) -> anyhow::Result<Self> {
+        let mut repo = None;
+        if let serde_yaml::Value::Mapping(m) = v {
+            if let Some(serde_yaml::Value::String(repo_s)) = m.get("repo") {
+                repo = Some(repo_s.as_str())
+            }
+        }
+        match repo {
+            Some("local") => Ok(Self::Local(LocalRepo::cfgv_validate(ctx, v)?)),
+            Some("meta") => Ok(Self::Meta(MetaRepo::cfgv_validate(ctx, v)?)),
+            _ => Ok(Self::Remote(RemoteRepo::cfgv_validate(ctx, v)?)),
+        }
+    }
 }
 
 #[allow(dead_code)]
